87455f38be80a4a1b1f5d3b5424bd5eae59f6d7f
Wraytheon <94151369+Wraytheon@users.noreply.github.com>
2023-02-15 17:36:06 -0500

    link will only appear if link data is present

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
-document.getElementById("submit").addEventListener("click", getFetch)
-function getFetch () {
+const url = "https://www.boredapi.com/api/activity";
+document.getElementById("submit").addEventListener("click", getFetch);
+
+function getFetch() {
   const searchInput = document.querySelector("input").value;
 
-
-  fetch("https://www.boredapi.com/api/activity")
-                    /*Gets query value from date input*/
+  fetch(url)
+    /*Gets query value from date input*/
     .then((res) => res.json()) // parse response as JSON
     .then((data) => {
-
       console.log(data);
+
       // const typeSelected = document.getElementById("type")
       // function selectType {
       //   const typeValue = typeSelected.options[typeSelected.selectedIndex].value
@@ -18,16 +19,22 @@ function getFetch () {
       // }
       document.getElementById("activity-name").innerText = data.activity;
       document.getElementById("activity-type").innerText = data.type;
-      document.getElementById("activity-participants").innerText = data.participants;
+      document.getElementById("activity-participants").innerText =
+        data.participants;
       document.getElementById("activity-price").innerText = data.price;
       document.getElementById("activity-accessibility").innerText = data.accessibility;
-      document.getElementById("activity-link").innerText = data.link;
+      const linkElement = document.querySelector(".link-el")
+      if(data.link === "") {
+        linkElement.classList.add("hide")
+      } else {
+        linkElement.classList.remove("hide")
+        document.getElementById("activity-link").innerText = data.link;
+      }
+
       /* Progress bar calc*/
       // document.getElementsByClassName("progress-bar").style.width = data.accessibility * 100 + "%"
-
     })
     .catch((err) => {
       console.log(`error ${err}`);
     });
-
 }
